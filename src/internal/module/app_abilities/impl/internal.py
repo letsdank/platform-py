825db7791d6755f9553ee215c97ceed5bb51b24d
LetsDank <ichi2209@icloud.com>
2024-10-02 16:34:26 +0300

    Implemented tariffication check

--- src/internal/module/app_abilities/impl/internal.py
+++ src/internal/module/app_abilities/impl/internal.py
@@ -10,3 +10,19 @@ def is_retail_in_service_mode():
         return True
 
     return False
+
+# Alias: РозницаВАвтономномРабочемМесте
+def is_retail_in_offline_work_place() -> bool:
+    """
+    Is retail in offline work place
+    :return: bool
+    """
+
+    # common.is_offline_work_place() performs loop cycle,
+    # because it initiates data_exchange_impl.get_exchange_plans() and
+    # data_exchange_smb.get_exchange_plans(), which calls common_smb.is_smb().
+    if not common.is_subordinate_ddb_node(): # TODO: Distributed Database
+        return False
+
+    result = constants.retail_in_offline_work_place.get()
+    return result
